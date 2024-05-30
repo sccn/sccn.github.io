15b40d855e4c5cf471833164211500ed6609ea5f
Dung Truong <dt.young112@gmail.com>
2024-05-30 14:32:53 -0400

    add SIFT plugin

---
+++ code/update_plugins.sh
@@ -0,0 +1,3 @@
+git -C /Users/dtyoung/Documents/EEGLAB/sccn.github.io/plugins/SIFT.wiki fetch origin master
+git -C /Users/dtyoung/Documents/EEGLAB/sccn.github.io/plugins/SIFT.wiki reset --hard origin/master
+python /Users/dtyoung/Documents/EEGLAB/sccn.github.io/code/SIFT.py /Users/dtyoung/Documents/EEGLAB/sccn.github.io/plugins/SIFT.wiki SIFT
---
+++ plugins/index.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: Plugins
+has_children: true
+has_toc: false
+nav_order: 7
+---
+This is a test
